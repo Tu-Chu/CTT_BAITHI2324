6b758c2a635cd972657b162b5959ae8ab375351b
Tu-Chu <tuchuthien2710@gmail.com>
2024-06-17 12:41:31 +0700

    baithi

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace CHUTHIENTU_639.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
